509405f9cf85c101707bdbf0de52ce2c991159e3
Prince Srivastava <pk.sri1212@gmail.com>
2025-03-26 01:18:52 +0530

    Python day 2

---
+++ 2nd.py
@@ -0,0 +1,20 @@
+a = 2 #integer
+b = 3.4 #float
+print( a+b)
+print(type(a)) #type of a
+print(type(b)) # type of b
+
+name = "John Doe" #string
+acount_balance = 1023039.23 #float
+Interest_rate = 12.1 #float
+Profit = acount_balance * Interest_rate / 100
+print(Profit)
+
+print(type(acount_balance)) #type of acount_balance
+
+account_type = "Savings" #string
+print(type(account_type))
+
+is_customer_alive = True #boolean
+print(type(is_customer_alive)) #type of is_customer_alive
+print( name , "is a customer and his account balance is", acount_balance ,".", "He has earned a profit of", Profit) #concatenation
